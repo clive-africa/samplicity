bee1ce9c1612d073408a13a86c328244dfe0a252
Clive <61520270+clive-africa@users.noreply.github.com>
2024-10-07 23:29:24 +0200

    Update conf.py
    Trying to fix some issues with the project structure

--- docs/conf.py
+++ docs/conf.py
@@ -1,70 +1,27 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
 
 # Add the project root directory to the Python path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-sys.path.insert(0, project_root)
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = "samplicity"
-copyright = "2024, Clive Hogarth"
-author = "Clive Hogarth"
-release = "0.0.1"
+project = 'samplicity'
+copyright = '2024, Clive Hogarth'
+author = 'Clive Hogarth'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.todo",
-    "sphinx.ext.viewcode",
-    "sphinx_rtd_theme",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
 ]
 
-todo_include_todos = True
-templates_path = ["_templates"]
-exclude_patterns = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
-
-autodoc_default_options = {
-    "members": True,
-    "undoc-members": True,
-    "show-inheritance": True,
-    "special-members": "__init__",
-    "ignore-module-all": True,
-    "exclude-members": "__weakref__",
-}
-
-html_theme_options = {
-    "display_version": True,
-    "prev_next_buttons_location": "bottom",
-    "style_external_links": False,
-    # Toc options
-    "collapse_navigation": False,
-    "sticky_navigation": True,
-    "navigation_depth": 4,
-    "includehidden": True,
-    "titles_only": False,
-}
-
-add_module_names = False
-autodoc_typehints = "description"
-
-# Ensure the samplicity directory is in the Python path
-samplicity_dir = os.path.join(project_root, 'samplicity')
-if os.path.exists(samplicity_dir):
-    sys.path.insert(0, samplicity_dir)
-else:
-    print(f"Warning: {samplicity_dir} does not exist. Please check your project structure.")
+# -- Extension configuration -------------------------------------------------
+autodoc_member_order = 'bysource'
